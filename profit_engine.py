cdca579dc32c7452200ffb31cfb2eeb1f07607ee
khanamcania33-max <khanamcania33@gmail.com>
2026-03-09 22:35:22 +0600

    Update profit_engine.py

--- profit_engine.py
+++ profit_engine.py
@@ -1,8 +1,6 @@
-def profit_sim(price):
-    cost = price*0.25
-    shipping = 4
-    fba = price*0.15
-    ppc = price*0.10
-    profit = price-(cost+shipping+fba+ppc)
-    margin = profit/price
-    return round(profit,2), round(margin*100,1)
+def score_product(row):
+    demand = 25 if row["Price"]*row["Reviews"]>50000 else 18 if row["Price"]*row["Reviews"]>25000 else 10
+    competition = 20 if row["Reviews"]<120 else 12
+    trend = 15 if row["Reviews"]>50 else 10
+    improvement = 10 if row["Rating"]<4.5 else 5
+    return demand + competition + trend + improvement
