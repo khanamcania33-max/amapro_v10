d3b408c1142ff012d5750c86211c5a8ab7461f30
khanamcania33-max <khanamcania33@gmail.com>
2026-03-09 21:59:35 +0600

    Update discovery_engine.py

--- discovery_engine.py
+++ discovery_engine.py
@@ -1,37 +1,33 @@
 import random
 import pandas as pd
-from faker import Faker
-
-fake = Faker()
 
 def discover_products(keywords):
 
-    rows = []
+    rows=[]
 
     for k in keywords:
 
-        price = round(random.uniform(40,150),2)
+        price=round(random.uniform(40,150),2)
 
-        sales = random.randint(50,1500)
+        sales=random.randint(50,1500)
 
-        revenue = price * sales
+        revenue=price*sales
 
-        reviews = random.randint(10,400)
+        reviews=random.randint(10,400)
 
-        rating = round(random.uniform(3.5,4.8),2)
+        rating=round(random.uniform(3.5,4.8),2)
 
-        keyword_volume = random.randint(2000,50000)
+        keyword_volume=random.randint(2000,50000)
 
         rows.append({
 
-        "keyword":k,
-        "price":price,
-        "monthly_sales":sales,
-        "revenue":revenue,
-        "reviews":reviews,
-        "rating":rating,
-        "keyword_volume":keyword_volume,
-        "asin":fake.bothify(text='B0#####')
+        "Niche":k,
+        "Price":price,
+        "Monthly Sales":sales,
+        "Revenue":revenue,
+        "Reviews":reviews,
+        "Rating":rating,
+        "Keyword Volume":keyword_volume
 
         })
 
