9000bbcca705970b6e42c9e6f683bfccf4528b4d
khanamcania33-max <khanamcania33@gmail.com>
2026-03-09 22:23:01 +0600

    Update scoring_engine.py

--- scoring_engine.py
+++ scoring_engine.py
@@ -1,13 +1,6 @@
 def score_product(row):
-
-    demand = 25 if row["Revenue"]>50000 else 18 if row["Revenue"]>25000 else 10
-
+    demand = 25 if row["Price"]*row["Reviews"]>50000 else 18 if row["Price"]*row["Reviews"]>25000 else 10
     competition = 20 if row["Reviews"]<120 else 12
-
-    trend = 15 if row["Monthly Sales"]>800 else 10
-
-    keyword = 15 if row["Keyword Volume"]>20000 else 8
-
-    improvement = 10 if row["Rating"]<4.3 else 5
-
-    return demand+competition+trend+keyword+improvement
+    trend = 15 if row["Reviews"]>50 else 10
+    improvement = 10 if row["Rating"]<4.5 else 5
+    return demand + competition + trend + improvement
