2fdc1c11cc00837c3cb0c7dc8e87d408f59d6fd1
khanamcania33-max <khanamcania33@gmail.com>
2026-03-09 22:22:31 +0600

    Update profit_engine.py

--- profit_engine.py
+++ profit_engine.py
@@ -1,15 +1,8 @@
 def profit_sim(price):
-
     cost = price*0.25
-
     shipping = 4
-
     fba = price*0.15
-
     ppc = price*0.10
-
     profit = price-(cost+shipping+fba+ppc)
-
     margin = profit/price
-
     return round(profit,2), round(margin*100,1)
